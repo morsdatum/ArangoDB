4d98b5325935b5c72db1da6232d2353a341706f7
User Nodba <nodba@seadragon.nix.bra>
2014-12-08 14:13:42 +0800

    add script to record compiling opts

---
+++ compile.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+export CC=clang
+export CXX=clang++
+export LINK=clang++
+
+export CFLAGS="-Wno-unused-private-field -Wno-unused-variable -Wno-nested-anon-types -Wno-unused-function"
+export LDFLAGS="-L/usr/local/lib -lexecinfo"
+export CPPFLAGS="-I/usr/local/include"
+
+gmake -j 4 
